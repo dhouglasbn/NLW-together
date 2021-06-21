ab8136b3e070dca42372ddb6763969931cde5686
dhouglas <dhouglasbn@gmail.com>
2021-06-21 11:19:05 -0300

    adding enviroment variables to firebaseConfig

--- letmeask/src/services/firebase.ts
+++ letmeask/src/services/firebase.ts
@@ -4,13 +4,13 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBTNmiQEb-Hwtnn6V2R_RxonF-wlUrBYgM",
-    authDomain: "letmeask-48a7e.firebaseapp.com",
-    databaseURL: "https://letmeask-48a7e-default-rtdb.firebaseio.com",
-    projectId: "letmeask-48a7e",
-    storageBucket: "letmeask-48a7e.appspot.com",
-    messagingSenderId: "428265752692",
-    appId: "1:428265752692:web:74e44a76ae359b6c8c2921"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 
 firebase.initializeApp(firebaseConfig);
