236d7fe6e4316326d81aaef1eaf798d70419a121
dhouglas <dhouglasbn@gmail.com>
2021-06-25 10:50:31 -0300

    just show answer & highlight button if it's not answered

--- letmeask/src/pages/AdminRoom.tsx
+++ letmeask/src/pages/AdminRoom.tsx
@@ -85,18 +85,23 @@ export function AdminRoom() {
                             isAnswered={question.isAnswered}
                             isHighlighted={question.isHighlighted}
                             >
-                                <button 
-                                type="button"
-                                onClick={() => handleCheckQuestionAsAnswered(question.id)}
-                                >
-                                    <img src={checkImg} alt="Marcar pergunta como respondida" />
-                                </button>
-                                <button 
-                                type="button"
-                                onClick={() => handleHighlightQuestion(question.id)}
-                                >
-                                    <img src={answerImg} alt="Dar destaque à pergunta" />
-                                </button>
+                                {/* usando aqui uma fragment, pro react serve como uma parent, mas não vai para o html */}
+                                {!question.isAnswered && (
+                                    <>
+                                        <button 
+                                            type="button"
+                                            onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                                        >
+                                            <img src={checkImg} alt="Marcar pergunta como respondida" />
+                                        </button>
+                                        <button 
+                                            type="button"
+                                            onClick={() => handleHighlightQuestion(question.id)}
+                                        >
+                                            <img src={answerImg} alt="Dar destaque à pergunta" />
+                                        </button>
+                                    </>
+                                )}  
                                 <button 
                                 type="button"
                                 onClick={() => handleDeleteQuestion(question.id)}
