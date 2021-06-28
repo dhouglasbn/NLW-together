e6ca61608abfa9d6da8771cd61ca037df84f13c6
dhouglas <dhouglasbn@gmail.com>
2021-06-28 10:34:33 -0300

    commenting services/firebase.ts

--- letmeask/src/services/firebase.ts
+++ letmeask/src/services/firebase.ts
@@ -3,6 +3,9 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
 
+// dados do firebase que estão salvos como variáveis de ambiente
+// também disponíveis nas configurações do projeto firebase
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -13,9 +16,13 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_APP_ID
   };
 
+// inicializando app com minha config
 firebase.initializeApp(firebaseConfig);
 
+// usando as autenticações que eu defini no projeto firebase
 const auth = firebase.auth();
+
+// usando a database que eu defini no projeto firebase
 const database = firebase.database();
 
 export { firebase, auth, database };
