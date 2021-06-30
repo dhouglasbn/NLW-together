82f8748dd2c34568a276a9a9399a8f53602fa737
dhouglas <dhouglasbn@gmail.com>
2021-06-30 11:29:35 -0300

    adding style to themes

---
+++ letmeask/src/styles/styled.d.ts
@@ -0,0 +1,14 @@
+declare module "styled-components" {
+    export interface DefaulTheme {
+        title: string,
+
+    colors: {
+        primary: string,
+        secondary: string,
+
+        background: string,
+        text: string
+    }
+    }
+
+}
