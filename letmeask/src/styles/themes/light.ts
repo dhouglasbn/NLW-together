e60ee4df34d704b556cb2ae118b9bac6c4f108d8
dhouglas <dhouglasbn@gmail.com>
2021-06-30 11:15:39 -0300

    installing styled-components, adding global style

---
+++ letmeask/src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+    title: "light",
+
+    colors: {
+        primary: "#835afd",
+        secondary: "#e559f9",
+
+        background: "#f8f8f8",
+        text: "#29292e"
+    }
+}
